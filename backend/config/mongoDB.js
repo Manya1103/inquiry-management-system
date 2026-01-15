7d9bf730c7b827f4bd93685c33652874d02ec3cf
Manya <manya111203@gmail.com>
2026-01-15 12:38:28 +0530

    update: project name

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         mongoose.connection.on("connected", () => console.log("MongoDB connected successfully"));
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/ai-image-generator`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/Inquiry-management-system`, )
     } catch (error) {
         console.log(`Error connecting to MongoDB: ${error.message}`);
     }
