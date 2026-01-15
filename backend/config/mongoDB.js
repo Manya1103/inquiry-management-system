58380108dda71a6ac8bdfbaeb0480121c620fbec
Manya <manya111203@gmail.com>
2026-01-15 11:50:12 +0530

    Add: frontend, backend, mongodb, tailwind CSS setup

---
+++ backend/config/mongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => console.log("MongoDB connected successfully"));
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/ai-image-generator`)
+    } catch (error) {
+        console.log(`Error connecting to MongoDB: ${error.message}`);
+    }
+}
+
+export default connectDB;
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/mongoDB.js";
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Allow requests from your front-end
+
+// Simple test route
+app.get("/", (req, res) => {
+  res.send("Hello from AI Image Generator Backend!");
+});
+
+// Function to start the server
+const Server = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB(); // Call the function from config/mongoDB.js
+
+    // Start listening for requests
+    const PORT = process.env.PORT || 3000; // Use port from .env or default to 3000
+    app.listen(PORT, () => {
+      console.log(
+        `Server is running successfully on port http://localhost:${PORT}`
+      );
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+Server(); // Run the function to connect DB and start server
